9c115cf761939f662f3cc8620aa8587e8f70f62a
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-05-01 04:28:22 -0400

    net: Only allow init netns to set default tcp cong to a restricted algo
    tcp_set_default_congestion_control() is netns-safe in that it writes
    to &net->ipv4.tcp_congestion_control, but it also sets
    ca->flags |= TCP_CONG_NON_RESTRICTED which is not namespaced.
    This has the unintended side-effect of changing the global
    net.ipv4.tcp_allowed_congestion_control sysctl, despite the fact that it
    is read-only: 97684f0970f6 ("net: Make tcp_allowed_congestion_control
    readonly in non-init netns")
    
    Resolve this netns "leak" by only allowing the init netns to set the
    default algorithm to one that is restricted. This restriction could be
    removed if tcp_allowed_congestion_control were namespace-ified in the
    future.
    
    This bug was uncovered with
    https://github.com/JonathonReinhart/linux-netns-sysctl-verify
    
    Fixes: 6670e1524477 ("tcp: Namespace-ify sysctl_tcp_default_congestion_control")
    Signed-off-by: Jonathon Reinhart <jonathon.reinhart@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_cong.c
+++ net/ipv4/tcp_cong.c
@@ -222,23 +222,25 @@ void tcp_cleanup_congestion_control(struct sock *sk)
 int tcp_set_default_congestion_control(const char *name)
 {
 	struct tcp_congestion_ops *ca;
-	int ret = -ENOENT;
+	const struct tcp_congestion_ops *prev;
+	int ret;
 
-	spin_lock(&tcp_cong_list_lock);
-	ca = tcp_ca_find(name);
-#ifdef CONFIG_MODULES
-	if (!ca && capable(CAP_NET_ADMIN)) {
-		spin_unlock(&tcp_cong_list_lock);
+	rcu_read_lock();
+	ca = tcp_ca_find_autoload(net, name);
+	if (!ca) {
+		ret = -ENOENT;
+	} else if (!try_module_get(ca->owner)) {
+		ret = -EBUSY;
+	} else if (!net_eq(net, &init_net) &&
+			!(ca->flags & TCP_CONG_NON_RESTRICTED)) {
+		/* Only init netns can set default to a restricted algorithm */
+		ret = -EPERM;
+	} else {
+		prev = xchg(&net->ipv4.tcp_congestion_control, ca);
+		if (prev)
+			module_put(prev->owner);
 
-		request_module("tcp_%s", name);
-		spin_lock(&tcp_cong_list_lock);
-		ca = tcp_ca_find(name);
-	}
-#endif
-
-	if (ca) {
-		ca->flags |= TCP_CONG_NON_RESTRICTED;	/* default is always allowed */
-		list_move(&ca->list, &tcp_cong_list);
+		ca->flags |= TCP_CONG_NON_RESTRICTED;
 		ret = 0;
 	}
 	spin_unlock(&tcp_cong_list_lock);
